66b9203e32842d1af84d891805aaf30e205f9ad0
Serdar Coşkun <coskun.serdar@gmail.com>
2022-03-22 19:47:23 +0300

    updated dependencies for own repository

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "AudioKitEX",
     platforms: [.macOS(.v10_13), .iOS(.v11), .tvOS(.v11)],
     products: [.library(name: "AudioKitEX", targets: ["AudioKitEX"])],
-    dependencies: [.package(url: "https://github.com/AudioKit/AudioKit", from: "5.3.0")],
+    dependencies: [.package(url: "https://github.com/overtune-app/AudioKit", .revision("77243f19117400dd4927b5ff1ee3b21a1d5ed11f"))],
     targets: [
         .target(name: "AudioKitEX", dependencies: ["AudioKit", "CAudioKitEX"]),
         .target(name: "CAudioKitEX", cxxSettings: [.headerSearchPath(".")]),
